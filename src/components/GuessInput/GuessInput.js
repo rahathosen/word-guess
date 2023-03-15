6285c507daf3303d40fa281e5ee62a49e69104be
rahathosen <rahathosen.me@gmail.com>
2023-03-15 19:14:24 +0600

    completed exercise 1 guess input

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handlerSubmit(event) {
+    event.preventDefault();
+
+    if (guess.length !== 5) {
+      window.alert("Ohh, Please enter exactly 5 characters. 💖");
+      return;
+    }
+
+    console.log({ guess });
+    setGuess("");
+  }
+  return (
+    <>
+      <form onSubmit={handlerSubmit} className="guess-input-wrapper">
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          required
+          minLength={5}
+          maxLength={5}
+          pattern="[a-zA-Z]{5}"
+          title="5 latter world"
+          id="guess-input"
+          type="text"
+          value={guess}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+        />
+      </form>
+    </>
+  );
+}
+
+export default GuessInput;
